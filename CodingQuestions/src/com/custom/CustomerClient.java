d251d3ddeaf463722fdc52ba562b09268015d49c
AshishSingh24356 <ashishsingh24356@gmail.com>
2024-06-11 09:30:46 +0530

    I have iterate the array list using different method

--- CodingQuestions/src/com/custom/CustomerClient.java
+++ CodingQuestions/src/com/custom/CustomerClient.java
@@ -1,7 +1,9 @@
 package com.custom;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class CustomerClient {
 	public static void main(String[] args) {
@@ -17,5 +19,37 @@ public class CustomerClient {
 		listOfCustomer.add(new Customer(9, "Amit", "9554024377", 24));
 		listOfCustomer.add(new Customer(10, "Ajay", "9554024355", 23));
 		System.out.println(listOfCustomer);
+		System.out.println('\n');
+
+		// Iterate through iterator
+		System.out.println("=============== Iterate through iterator ===================");
+		Iterator<Customer> itr = listOfCustomer.iterator();
+		while (itr.hasNext()) {
+			Customer customer = itr.next();
+			System.out.println(customer);
+
+		}
+
+		// Iterate through advance for loop
+		System.out.println('\n');
+		System.out.println("=============== Iterate through advance for loop ===================");
+		for (Customer customer : listOfCustomer) {
+			System.out.println(customer);
+		}
+
+		// Iterate through ListIterator cursor
+		System.out.println('\n');
+		System.out.println("=============== Iterate through ListIterator cursor ===================");
+		ListIterator<Customer> listIterator = listOfCustomer.listIterator();
+		while (listIterator.hasNext()) {
+			Customer customer = listIterator.next();
+			System.out.println(customer);
+		}
+
+		// Iterate through java 8
+		System.out.println('\n');
+		System.out.println("=============== Iterate through java 8===================");
+		listOfCustomer.stream().forEach(System.out::println);
+
 	}
 }
